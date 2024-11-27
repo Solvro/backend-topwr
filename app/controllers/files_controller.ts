ddef8b06102839d5ce58b911dd36dc5bae493034
Kamil Fedio <kamilf827@gmail.com>
2024-11-27 14:59:52 +0100

    feat: add status codes

--- app/controllers/files_controller.ts
+++ app/controllers/files_controller.ts
@@ -2,18 +2,19 @@ import type { HttpContext } from '@adonisjs/core/http'
 import drive from '@adonisjs/drive/services/main'
 
 export default class PhotosController {
-  async post({ request }: HttpContext) {
+  async post({ request, response }: HttpContext) {
     const file = request.file('file')
     const key = `${new Date().getTime()}.${file?.extname}`
     if (file) {
-      // await drive.use().put(key, file.tmpPath ?? '')
       await file.moveToDisk(key)
+    } else {
+      return response.badRequest('File is required')
     }
-    return key
+    return response.created({ key })
   }
 
-  async get({ params }: HttpContext) {
+  async get({ params, response }: HttpContext) {
     const { key } = params
-    return drive.use().getUrl(key)
+    return response.status(200).send(await drive.use().getUrl(key))
   }
 }
