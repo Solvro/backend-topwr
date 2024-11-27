0fb9484e1d05296c4c1a2894fecff54202fdcf9c
Kamil Fedio <82204376+kamilfedio@users.noreply.github.com>
2024-11-27 21:18:40 +0100

    feat: create storage for files (#25)
    * chore: upgrade packages
    
    * feat: add drive config for localfile system
    
    * docs: add folder to gitignore
    
    * feat: add app url env variable
    
    * docs: create readme with example of usage of drive
    
    * chore: format
    
    * chore: format
    
    * feat: eample route with controller
    
    * docs: update some env
    
    * feat: add file controller
    
    * feat: add route file
    
    * feat: add status codes
    
    * feat: add uuid generator for file names

--- start/routes.ts
+++ start/routes.ts
@@ -7,10 +7,17 @@
 |
 */
 
+import env from '#start/env'
 import router from '@adonisjs/core/services/router'
+const FilesController = () => import('#controllers/files_controller')
 
 router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
+  return { appName: env.get('APP_NAME'), version: env.get('APP_VERSION') }
 })
+
+router
+  .group(() => {
+    router.get('files/:key', [FilesController, 'get'])
+    router.post('files', [FilesController, 'post'])
+  })
+  .prefix('api/v1')
