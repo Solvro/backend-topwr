bbede41835ff46b86914179114e607cfcd6455a4
Dawid Góra <mikuu252@gmail.com>
2024-11-28 14:17:04 +0100

    chore: add types to LinkType

--- app/enums/link_type.ts
+++ app/enums/link_type.ts
@@ -1,1 +1,15 @@
-export enum LinkType {}
+export enum LinkType {
+  Default = 'default',
+  Facebook = 'facebook',
+  Instagram = 'instagram',
+  LinkedIn = 'linkedin',
+  Mail = 'mailto:',
+  YouTube = 'youtu',
+  GitHub = 'github',
+  TopwrBuildings = 'topwr:buildings',
+  Phone = 'tel',
+  X = 'https://x.com',
+  TikTok = 'tiktok',
+  Discord = 'discord',
+  Twitch = 'twitch',
+}
