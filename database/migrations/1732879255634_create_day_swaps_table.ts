beecd74582b68460dce09dc06719da6284b6d8c1
mateusz-plaska <147147104+mateusz-plaska@users.noreply.github.com>
2024-11-30 19:42:23 +0100

    feat: add adonis migrations and models for academic calendar (#40)
    * feat: add migrations needed for academic_calendar
    
    * feat: add models for academic_calendar
    
    * chore: add db schema for academic_calendar
    
    * chore: fix prettier

---
+++ database/migrations/1732879255634_create_day_swaps_table.ts
@@ -0,0 +1,35 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+import { Weekday } from '../../app/enums/weekday.js'
+
+export default class extends BaseSchema {
+  protected tableName = 'day_swaps'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+      table.bigInteger('academic_calendar_id').unsigned().notNullable()
+      table.foreign('academic_calendar_id').references('academic_calendars.id').onDelete('CASCADE')
+
+      table.date('date').notNullable()
+
+      table
+        .enum('changed_weekday', Object.values(Weekday), {
+          useNative: true,
+          enumName: 'weekday',
+          existingType: false,
+        })
+        .notNullable()
+
+      table.boolean('changed_day_is_even').notNullable()
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.raw('DROP TYPE IF EXISTS "weekday"')
+    this.schema.dropTable(this.tableName)
+  }
+}
