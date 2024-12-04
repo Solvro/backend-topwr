cc5f863a27f614b91af6b123bbf103b5e4937bf7
karolwolek <karolwolek06@gmail.com>
2024-12-04 23:18:25 +0100

    feat/buildings controller and routes (#52)
    * feat: added routes for buildings and campuses
    
    * fix: fixed building model for some names to mirror their migration names
    
    * feat: simple seeder for buildings and campuses to test their functionality
    
    * refactor: added relation hasMany in campus for Buildings

--- app/models/campus.ts
+++ app/models/campus.ts
@@ -1,5 +1,7 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import Building from './building.js'
+import type { HasMany } from '@adonisjs/lucid/types/relations'
 
 export default class Campus extends BaseModel {
   @column({ isPrimary: true })
@@ -16,4 +18,8 @@ export default class Campus extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime
+
+  @hasMany(() => Building)
+  declare buildings: HasMany<typeof Building>;
+  
 }
---
+++ database/seeders/building_seeder.ts
@@ -0,0 +1,49 @@
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
+import Building from '#models/building'
+import { BuildingIcon } from '../../app/enums/building_icon.js'
+
+
+
+
+export default class BuldingSeeder extends BaseSeeder {
+  async run() {
+    await Building.createMany([
+      {
+        identifier: 'B001',
+        specialName: 'Main Admin Building',
+        iconType: BuildingIcon.Icon,
+        campusId: 2,
+        addressLine1: '123 University Ave',
+        addressLine2: 'Building A',
+        latitude: 40.7128,
+        longitude: -74.0060,
+        haveFood: true,
+        cover: 'https://example.com/covers/building1.jpg'
+      },
+      {
+        identifier: 'B002',
+        specialName: 'Engineering Block',
+        iconType: BuildingIcon.Icon,
+        campusId: 3,
+        addressLine1: '124 University Ave',
+        addressLine2: 'Building B',
+        latitude: 40.7129,
+        longitude: -74.0061,
+        haveFood: false,
+        cover: 'https://example.com/covers/building2.jpg'
+      },
+      {
+        identifier: 'B003',
+        specialName: null,
+        iconType: BuildingIcon.Icon,
+        campusId: 2,
+        addressLine1: '500 Science Park',
+        addressLine2: 'kolowe sny',
+        latitude: 37.7749,
+        longitude: -122.4194,
+        haveFood: true,
+        cover: 'https://example.com/covers/building3.jpg'
+      },
+    ])
+  }
+}
