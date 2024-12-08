a20f0350825a163df19c23a9f3ef682cfb89d08c
Bartosz Słowik <bartoldo2004@gmail.com>
2024-12-08 21:14:40 +0100

    feat: links and tags

---
+++ database/migrations/1733687303819_create_student_organisations_student_organisation_tags_table.ts
@@ -0,0 +1,21 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'student_organisations_student_organisation_tags'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.text('tag').notNullable()
+      table.integer('student_organisation_id').unsigned().notNullable()
+      table.timestamp('created_at').notNullable()
+      table.timestamp('updated_at').notNullable()
+      table.primary(['tag', 'student_organisation_id'])
+      table.foreign('student_organisation_id').references('student_organisations.id').onDelete('CASCADE')
+      table.foreign('tag').references('student_organisation_tags.tag').onDelete('CASCADE')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
---
+++ database/migrations/1733688213674_create_student_organisation_links_table.ts
@@ -0,0 +1,35 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'student_organisation_links'
+  protected linkTypes = [
+    'website',
+    'facebook',
+    'instagram',
+    'linkedin',
+    'mailto:',
+    'youtube',
+    'github',
+    'twitter',
+    'tiktok',
+    'discord',
+    'twitch',
+  ]
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('student_organisation_id').unsigned().notNullable()
+      table.enum('type', this.linkTypes, {useNative: true, enumName: 'organization_link_type', existingType: false}).notNullable()
+      table.text('link').notNullable()
+      table.timestamp('created_at').notNullable()
+      table.timestamp('updated_at').notNullable()
+      table.foreign('student_organisation_id').references('student_organisations.id').onDelete('CASCADE')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+    this.schema.raw('DROP TYPE IF EXISTS "organization_link_type"')
+  }
+}
