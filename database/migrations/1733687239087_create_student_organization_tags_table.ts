d09a228f20f96e39c0bda9c8ba9a41d81c533e1a
Bartosz Słowik <bartoldo2004@gmail.com>
2024-12-11 13:08:53 +0100

    refactor: prettier

--- database/migrations/1733687239087_create_student_organization_tags_table.ts
+++ database/migrations/1733687239087_create_student_organization_tags_table.ts
@@ -1,18 +1,18 @@
-import { BaseSchema } from '@adonisjs/lucid/schema'
+import { BaseSchema } from "@adonisjs/lucid/schema";
 
 export default class extends BaseSchema {
-  protected tableName = 'student_organization_tags'
+  protected tableName = "student_organization_tags";
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.text('tag').notNullable()
-      table.timestamp('created_at').notNullable()
-      table.timestamp('updated_at').notNullable()
-      table.primary(['tag'])
-    })
+      table.text("tag").notNullable();
+      table.timestamp("created_at").notNullable();
+      table.timestamp("updated_at").notNullable();
+      table.primary(["tag"]);
+    });
   }
 
   async down() {
-    this.schema.dropTable(this.tableName)
+    this.schema.dropTable(this.tableName);
   }
 }
--- database/migrations/1733687303819_create_student_organizations_student_organization_tags_table.ts
+++ database/migrations/1733687303819_create_student_organizations_student_organization_tags_table.ts
@@ -1,24 +1,27 @@
-import { BaseSchema } from '@adonisjs/lucid/schema'
+import { BaseSchema } from "@adonisjs/lucid/schema";
 
 export default class extends BaseSchema {
-  protected tableName = 'student_organizations_student_organization_tags'
+  protected tableName = "student_organizations_student_organization_tags";
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.text('tag').notNullable()
-      table.integer('student_organization_id').unsigned().notNullable()
-      table.timestamp('created_at').notNullable()
-      table.timestamp('updated_at').notNullable()
-      table.primary(['tag', 'student_organization_id'])
+      table.text("tag").notNullable();
+      table.integer("student_organization_id").unsigned().notNullable();
+      table.timestamp("created_at").notNullable();
+      table.timestamp("updated_at").notNullable();
+      table.primary(["tag", "student_organization_id"]);
       table
-        .foreign('student_organization_id')
-        .references('student_organizations.id')
-        .onDelete('CASCADE')
-      table.foreign('tag').references('student_organization_tags.tag').onDelete('CASCADE')
-    })
+        .foreign("student_organization_id")
+        .references("student_organizations.id")
+        .onDelete("CASCADE");
+      table
+        .foreign("tag")
+        .references("student_organization_tags.tag")
+        .onDelete("CASCADE");
+    });
   }
 
   async down() {
-    this.schema.dropTable(this.tableName)
+    this.schema.dropTable(this.tableName);
   }
 }
