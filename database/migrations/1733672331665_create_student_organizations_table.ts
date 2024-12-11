1532027a01ae1d6013cd4444d560a4afbd8b57cb
Bartosz Słowik <96744094+Batory12@users.noreply.github.com>
2024-12-11 15:10:12 +0100

    feat/organizations migrations (#56)
    * organizations table
    
    * fix: organizations table
    
    * feat: links and tags
    
    * add db diagram
    
    * refactor: prettier
    
    * fix: fixes from pr.
    unified names
    
    * refactor: camel case
    
    * refactor: prettier

---
+++ database/migrations/1733672331665_create_student_organizations_table.ts
@@ -0,0 +1,51 @@
+import { BaseSchema } from "@adonisjs/lucid/schema";
+
+export default class extends BaseSchema {
+  protected tableName = "student_organizations";
+  protected sources = ["student_department", "manual", "pwr_active"];
+  protected organizationType = [
+    "scientific_circle",
+    "student_organization",
+    "student_medium",
+    "culture_agenda",
+    "student_council",
+  ];
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments("id");
+      table.text("name").notNullable();
+      table.integer("department_id").unsigned().notNullable();
+      table.text("logo").nullable();
+      table.text("cover").nullable();
+      table.text("description").nullable();
+      table.text("short_description").nullable();
+      table.boolean("cover_preview").defaultTo(false).notNullable();
+      table
+        .enum("source", this.sources, {
+          useNative: true,
+          enumName: "source",
+          existingType: false,
+        })
+        .notNullable();
+      table
+        .enum("organization_type", this.organizationType, {
+          useNative: true,
+          enumName: "organization_type",
+          existingType: false,
+        })
+        .notNullable();
+      table.timestamp("created_at").notNullable();
+      table.timestamp("updated_at").notNullable();
+      table
+        .foreign("department_id")
+        .references("departments.id")
+        .onDelete("RESTRICT");
+    });
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName);
+    this.schema.raw('DROP TYPE IF EXISTS "source"');
+    this.schema.raw('DROP TYPE IF EXISTS "organization_type"');
+  }
+}
---
+++ database/migrations/1733688213674_create_student_organization_links_table.ts
@@ -0,0 +1,45 @@
+import { BaseSchema } from "@adonisjs/lucid/schema";
+
+export default class extends BaseSchema {
+  protected tableName = "student_organization_links";
+  protected linkTypes = [
+    "default",
+    "facebook",
+    "instagram",
+    "linkedin",
+    "mailto:",
+    "youtu",
+    "github",
+    "topwr:buildings",
+    "tel",
+    "https://x.com",
+    "tiktok",
+    "discord",
+    "twitch",
+  ];
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments("id");
+      table.integer("student_organization_id").unsigned().notNullable();
+      table
+        .enum("type", this.linkTypes, {
+          useNative: true,
+          enumName: "link_type",
+          existingType: true,
+        })
+        .notNullable();
+      table.text("link").notNullable();
+      table.timestamp("created_at").notNullable();
+      table.timestamp("updated_at").notNullable();
+      table
+        .foreign("student_organization_id")
+        .references("student_organizations.id")
+        .onDelete("CASCADE");
+    });
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName);
+  }
+}
