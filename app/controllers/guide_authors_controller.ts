40288e48c16cf684326130f8f45125583ec770d7
Mateusz Stankiewicz <matistan05@gmail.com>
2025-01-19 20:33:03 +0100

    feat/faq controllers and routes (#80)
    * feat: add faq controllers
    
    * feat: update models
    
    * feat: add routes for faq section
    
    * fix: add @typedModel decorator
    
    * fix: move relations to readonly prop
    
    * fix: fix relation definition between guide articles and questions
    
    ---------
    
    Co-authored-by: mini-bomba <55105495+mini-bomba@users.noreply.github.com>

---
+++ app/controllers/guide_authors_controller.ts
@@ -0,0 +1,44 @@
+import type { HttpContext } from "@adonisjs/core/http";
+
+import GuideAuthor from "#models/guide_author";
+import { paginationValidator } from "#validators/pagination";
+import { showValidator } from "#validators/show";
+
+export default class GuideAuthorsController {
+  protected readonly relations = ["guideArticles"];
+  /**
+   * Display a list of resource
+   */
+  async index({ request }: HttpContext) {
+    const { page, limit } = await request.validateUsing(paginationValidator);
+    const baseQuery = GuideAuthor.query().withScopes((scopes) => {
+      scopes.handleSearchQuery(request.qs());
+      scopes.preloadRelations(request.only(this.relations));
+      scopes.handleSortQuery(request.input("sort"));
+    });
+    let authors;
+    if (page !== undefined) {
+      authors = await baseQuery.paginate(page, limit ?? 10);
+    } else {
+      authors = { data: await baseQuery };
+    }
+    return authors;
+  }
+
+  /**
+   * Show individual record
+   */
+  async show({ request }: HttpContext) {
+    const {
+      params: { id },
+    } = await request.validateUsing(showValidator);
+    const author = await GuideAuthor.query()
+      .withScopes((scopes) => {
+        scopes.preloadRelations(request.only(this.relations));
+      })
+      .where("id", id)
+      .firstOrFail();
+
+    return { data: author };
+  }
+}
--- app/models/guide_question.ts
+++ app/models/guide_question.ts
@@ -3,8 +3,20 @@ import { DateTime } from "luxon";
 import { BaseModel, belongsTo, column } from "@adonisjs/lucid/orm";
 import * as relations from "@adonisjs/lucid/types/relations";
 
+import { typedModel } from "#decorators/typed_model";
 import GuideArticle from "#models/guide_article";
+import { preloadRelations } from "#scopes/preload_helper";
+import { handleSearchQuery } from "#scopes/search_helper";
+import { handleSortQuery } from "#scopes/sort_helper";
 
+@typedModel({
+  id: "number",
+  title: "string",
+  answer: "string",
+  articleId: "number",
+  createdAt: "DateTime",
+  updatedAt: "DateTime",
+})
 export default class GuideQuestion extends BaseModel {
   @column({ isPrimary: true })
   declare id: number;
@@ -24,6 +36,14 @@ export default class GuideQuestion extends BaseModel {
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime;
 
-  @belongsTo(() => GuideArticle)
+  @belongsTo(() => GuideArticle, {
+    foreignKey: "articleId",
+  })
   declare guideArticle: relations.BelongsTo<typeof GuideArticle>;
+
+  static preloadRelations = preloadRelations(GuideQuestion);
+
+  static handleSearchQuery = handleSearchQuery(GuideQuestion);
+
+  static handleSortQuery = handleSortQuery(GuideQuestion);
 }
