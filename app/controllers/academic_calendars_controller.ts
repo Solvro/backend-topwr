b08d78a504b8606855841ddc463a360a87dadd31
Mateusz Płaska <plaskamateuszi8@gmail.com>
2025-01-19 14:06:13 +0100

    feat: add controllers for academic_calendar, holiday and day_swap

---
+++ app/controllers/academic_calendars_controller.ts
@@ -0,0 +1,45 @@
+import type { HttpContext } from "@adonisjs/core/http";
+
+import AcademicCalendar from "#models/academic_calendar";
+import { paginationValidator } from "#validators/pagination";
+import { showValidator } from "#validators/show";
+
+export default class AcademicCalendarsController {
+  protected readonly relations = ["holidays", "daySwaps"];
+
+  /**
+   * Display a list of resource
+   */
+  async index({ request }: HttpContext) {
+    const { page, limit } = await request.validateUsing(paginationValidator);
+    const baseQuery = AcademicCalendar.query().withScopes((scopes) => {
+      scopes.handleSearchQuery(request.qs());
+      scopes.preloadRelations(request.only(this.relations));
+      scopes.handleSortQuery(request.input("sort"));
+    });
+    let academicCalendars;
+    if (page !== undefined) {
+      academicCalendars = await baseQuery.paginate(page, limit ?? 10);
+    } else {
+      academicCalendars = { data: await baseQuery };
+    }
+    return academicCalendars;
+  }
+
+  /**
+   * Show individual record
+   */
+  async show({ request }: HttpContext) {
+    const {
+      params: { id },
+    } = await request.validateUsing(showValidator);
+    const academicCalendar = await AcademicCalendar.query()
+      .withScopes((scopes) => {
+        scopes.preloadRelations(request.only(this.relations));
+      })
+      .where("id", id)
+      .firstOrFail();
+
+    return { data: academicCalendar };
+  }
+}
---
+++ app/controllers/holidays_controller.ts
@@ -0,0 +1,45 @@
+import type { HttpContext } from "@adonisjs/core/http";
+
+import Holiday from "#models/holiday";
+import { paginationValidator } from "#validators/pagination";
+import { showValidator } from "#validators/show";
+
+export default class HolidaysController {
+  protected readonly relations = ["academicCalendar"];
+
+  /**
+   * Display a list of resource
+   */
+  async index({ request }: HttpContext) {
+    const { page, limit } = await request.validateUsing(paginationValidator);
+    const baseQuery = Holiday.query().withScopes((scopes) => {
+      scopes.handleSearchQuery(request.qs());
+      scopes.preloadRelations(request.only(this.relations));
+      scopes.handleSortQuery(request.input("sort"));
+    });
+    let holidays;
+    if (page !== undefined) {
+      holidays = await baseQuery.paginate(page, limit ?? 10);
+    } else {
+      holidays = { data: await baseQuery };
+    }
+    return holidays;
+  }
+
+  /**
+   * Show individual record
+   */
+  async show({ request }: HttpContext) {
+    const {
+      params: { id },
+    } = await request.validateUsing(showValidator);
+    const academicCalendar = await Holiday.query()
+      .withScopes((scopes) => {
+        scopes.preloadRelations(request.only(this.relations));
+      })
+      .where("id", id)
+      .firstOrFail();
+
+    return { data: academicCalendar };
+  }
+}
