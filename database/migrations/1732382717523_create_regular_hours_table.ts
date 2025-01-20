a3bc51402967c4e0780322a32f1ed8e8f60a8cfe
Mateusz Płaska <plaskamateuszi8@gmail.com>
2025-01-20 20:54:40 +0100

    fix: fix migrations types

--- database/migrations/1732382717523_create_regular_hours_table.ts
+++ database/migrations/1732382717523_create_regular_hours_table.ts
@@ -2,16 +2,31 @@ import { BaseSchema } from "@adonisjs/lucid/schema";
 
 export default class extends BaseSchema {
   protected tableName = "regular_hours";
+  protected weekdays = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments("id");
 
-      table.integer("week_day").checkBetween([1, 7]).notNullable();
+      table
+        .enum("week_day", this.weekdays, {
+          useNative: true,
+          enumName: "week_day",
+          existingType: false,
+        })
+        .notNullable();
       table.time("open_time").notNullable();
       table.time("close_time").notNullable();
 
-      table.bigInteger("library_id").unsigned().notNullable();
+      table.integer("library_id").unsigned().notNullable();
       table
         .foreign("library_id")
         .references("libraries.id")
@@ -24,5 +39,6 @@ export default class extends BaseSchema {
 
   async down() {
     this.schema.dropTable(this.tableName);
+    this.schema.raw('DROP TYPE IF EXISTS "week_day"');
   }
 }
