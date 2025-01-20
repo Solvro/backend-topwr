7947aeaaaca316978c807866bbb8f7bbdf0bf85a
Mateusz Płaska <plaskamateuszi8@gmail.com>
2025-01-20 20:56:05 +0100

    fix: fix models types and column_names

--- app/models/aed.ts
+++ app/models/aed.ts
@@ -15,10 +15,10 @@ export default class Aed extends BaseModel {
   @column()
   declare longitude: number;
 
-  @column()
+  @column({ columnName: "address_line1" })
   declare addressLine1: string | null;
 
-  @column()
+  @column({ columnName: "address_line2" })
   declare addressLine2: string | null;
 
   @column()
