baae2bc23dfeef51d2aa789170575dd748820969
Mateusz Płaska <plaskamateuszi8@gmail.com>
2025-01-20 21:00:42 +0100

    feat: add library controllers and update others

---
+++ app/controllers/libraries_controller.ts
@@ -0,0 +1,44 @@
+import Library from '#models/library';
+import { paginationValidator } from '#validators/pagination';
+import { showValidator } from '#validators/show';
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class LibrariesController {
+    protected readonly relations = ["regularHours", "specialHours", "building"];
+
+    /**
+     * Display a list of resource
+     */
+    async index({ request }: HttpContext) {
+        const { page, limit } = await request.validateUsing(paginationValidator);
+        const baseQuery = Library.query().withScopes((scopes) => {
+            scopes.handleSearchQuery(request.qs());
+            scopes.preloadRelations(request.only(this.relations));
+            scopes.handleSortQuery(request.input("sort"));
+        });
+        let libraries;
+        if (page !== undefined) {
+            libraries = await baseQuery.paginate(page, limit ?? 10);
+        } else {
+            libraries = { data: await baseQuery };
+        }
+        return libraries;
+    }
+
+    /**
+     * Show individual record
+     */
+    async show({ request }: HttpContext) {
+        const {
+        params: { id },
+        } = await request.validateUsing(showValidator);
+        const library = await Library.query()
+        .withScopes((scopes) => {
+            scopes.preloadRelations(request.only(this.relations));
+        })
+        .where("id", id)
+        .firstOrFail();
+
+        return { data: library };
+    }
+}
