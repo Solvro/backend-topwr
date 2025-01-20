01385eb7dafa317426b23ee7170e80367fa94e8f
Jakub Czajkowski <and.jj.an@gmail.com>
2025-01-20 17:00:42 +0100

    feat: models

---
+++ app/models/about_us_general_link.ts
@@ -0,0 +1,30 @@
+import { DateTime } from "luxon";
+
+import { BaseModel, column } from "@adonisjs/lucid/orm";
+
+import { typedModel } from "#decorators/typed_model";
+import { LinkType } from "#enums/link_type";
+
+@typedModel({
+  id: "number",
+  linkType: "string",
+  link: "string",
+  createdAt: "DateTime",
+  updatedAt: "DateTime",
+})
+export default class AboutUsGeneralLink extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number;
+
+  @column()
+  declare linkType: LinkType;
+
+  @column()
+  declare link: string;
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime;
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime;
+}
