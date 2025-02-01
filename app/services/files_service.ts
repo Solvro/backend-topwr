5a318f9250ab33a4a80cc8b3d5a898696606d5c5
Kamil Fedio <kamilf827@gmail.com>
2025-02-01 22:54:23 +0100

    feat: add throw an error feature

--- app/services/files_service.ts
+++ app/services/files_service.ts
@@ -3,32 +3,35 @@ import { randomUUID } from "node:crypto";
 import { MultipartFile } from "@adonisjs/core/types/bodyparser";
 import drive from "@adonisjs/drive/services/main";
 
+import {
+  FileServiceFileReadError,
+  FileServiceFileUploadError,
+} from "#exceptions/file_service_errors";
+
 export default class FilesService {
-  async uploadFile(file: MultipartFile): Promise<string | Error> {
+  async uploadFile(file: MultipartFile): Promise<string> {
     // Upload file to storage
     const key = `${randomUUID()}.${file.extname}`;
     try {
       await file.moveToDisk(key);
       return key;
     } catch (error) {
-      return error;
+      const err = error as Error;
+      throw new FileServiceFileUploadError(err.message, err.stack);
     }
   }
 
-  async getFileUrl(key: string): Promise<string | Error> {
+  async getFileUrl(key: string): Promise<string> {
     // Get file URL from storage
     try {
       return await drive.use().getUrl(key);
     } catch (error) {
-      return error;
+      const err = error as Error;
+      throw new FileServiceFileReadError(err.message, err.stack);
     }
   }
-  async deleteFile(key: string): Promise<void | Error> {
+  async deleteFile(key: string): Promise<void> {
     // Delete file from storage
-    try {
-      await drive.use().delete(key);
-    } catch (error) {
-      return error;
-    }
+    await drive.use().delete(key);
   }
 }
