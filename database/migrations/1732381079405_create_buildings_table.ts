c028b8f7068da5595865e9accca5d95a307abecd
karolwolek <karolwolek06@gmail.com>
2025-02-20 23:10:28 +0100

    feat: add digitalGuide columns in Buildings

--- database/migrations/1732381079405_create_buildings_table.ts
+++ database/migrations/1732381079405_create_buildings_table.ts
@@ -17,7 +17,7 @@ export default class extends BaseSchema {
         })
         .notNullable();
 
-      table.bigInteger("campus_id").unsigned().notNullable();
+      table.integer("campus_id").unsigned().notNullable();
       table
         .foreign("campus_id")
         .references("id")
@@ -30,6 +30,28 @@ export default class extends BaseSchema {
       table.decimal("longitude").notNullable();
       table.boolean("have_food").defaultTo(false);
       table.text("cover").notNullable();
+      table.text("external_digital_guide_mode").nullable();
+      table.text("external_digital_guide_id_or_url").nullable();
+      table.check(
+        `
+        (
+          external_digital_guide_mode = 'web_url'
+          AND external_digital_guide_id_or_url ~ '^([a-zA-Z]+)://.*$' 
+        ) 
+        OR
+        (
+          external_digital_guide_mode <> 'web_url'
+          AND external_digital_guide_id_or_url ~ '^[0-9]+$' 
+        )
+        OR
+        (
+          external_digital_guide_mode IS NULL
+          AND external_digital_guide_id_or_url IS NULL
+        )
+      `,
+        [],
+        "digital_guide_format_check",
+      );
 
       table.timestamp("created_at");
       table.timestamp("updated_at");
