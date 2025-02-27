de6db374143dbd70745d93539207310fe7e23a1e
Mateusz Stankiewicz <matistan05@gmail.com>
2025-02-27 21:37:48 +0100

    fix: better scrapper description

--- commands/scraper_faq.ts
+++ commands/scraper_faq.ts
@@ -5,7 +5,7 @@ import { faqScript } from "../scripts/faq_script.js";
 
 export default class ScraperFaq extends BaseCommand {
   static commandName = "scraper:faq";
-  static description = "";
+  static description = "This command runs the FAQ scraper script.";
 
   static options: CommandOptions = {
     startApp: true,
