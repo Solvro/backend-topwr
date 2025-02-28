e1bcafecf1e67c2875133aba34a6881139f2d55f
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2025-02-28 22:29:39 +0100

    refactor: make detectLinkType a public function on #enums/link_type
    also adds a wrapper with the same name on the `BaseScraperModule` which
    automatically logs any returned warnings
    
    (cherry picked from commit 4d5a23f4c3d1054e7b0dc0ec55891286ebd537f6)

--- app/enums/link_type.ts
+++ app/enums/link_type.ts
@@ -31,3 +31,58 @@ export const linkTypeEnumsValues = {
     { value: LinkType.Twitch, label: "Twitch" },
   ],
 };
+
+const linkDomains: [string, LinkType][] = [
+  ["facebook.com", LinkType.Facebook],
+  ["instagram.com", LinkType.Instagram],
+  ["linkedin.com", LinkType.LinkedIn],
+  ["youtube.com", LinkType.YouTube],
+  ["github.com", LinkType.GitHub],
+  ["x.com", LinkType.X],
+  ["twitter.com", LinkType.X],
+  ["discord.com", LinkType.Discord],
+  ["discord.gg", LinkType.Discord],
+  ["tiktok.com", LinkType.TikTok],
+  ["twitch.tv", LinkType.Twitch],
+];
+
+/**
+ * Detects the link type of the given URL.
+ *
+ * This should never throw or return null/undefined - instead it may include the `warning` field in the result if any issues are encountered.
+ * Make sure to check if this field is not undefined and handle the warning appropriately (such as by logging it)
+ * @param link - the URL to examine
+ * @returns an object with the detected link type and an optional warning
+ */
+export function detectLinkType(link: string): {
+  type: LinkType;
+  warning?: string;
+} {
+  let url;
+  try {
+    url = new URL(link);
+  } catch {
+    return {
+      type: LinkType.Default,
+      warning: `Failed to parse social link '${link}' - assigned the Default linktype`,
+    };
+  }
+  if (url.protocol === "mailto:") {
+    return { type: LinkType.Mail };
+  }
+  if (url.protocol !== "https:" && url.protocol !== "http:") {
+    return {
+      type: LinkType.Default,
+      warning: `Encountered an unknown protocol '${url.protocol}' in social link '${link}' - assigned the Default linktype`,
+    };
+  }
+  for (const [domain, type] of linkDomains) {
+    if (url.host === domain || url.host.endsWith(`.${domain}`)) {
+      return { type };
+    }
+  }
+  return {
+    type: LinkType.Default,
+    warning: `Social link '${link}' matched no domains - assigned the Default linktype`,
+  };
+}
