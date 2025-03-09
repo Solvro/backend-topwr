8cb81967da14c963fac806e1196daeceda54478c
Mateusz Stankiewicz <matistan05@gmail.com>
2025-03-09 11:21:49 +0100

    fix: create proper scraper module and fix some minor issues

--- database/scrapers/faq_script.ts
+++ database/scrapers/faq_script.ts
@@ -1,8 +1,7 @@
 import { DateTime } from "luxon";
 import { Readable } from "node:stream";
 
-import logger from "@adonisjs/core/services/logger";
-
+import { BaseScraperModule } from "#commands/db_scrape";
 import GuideArticle from "#models/guide_article";
 import GuideQuestion from "#models/guide_question";
 import FilesService from "#services/files_service";
@@ -42,7 +41,8 @@ interface PivotTable {
 
 interface ImageMetadata {
   data: {
-    type: string;
+    filename_disk: string | null;
+    filename_download: string | null;
   };
 }
 
@@ -67,124 +67,143 @@ async function uploadImage(imageUrl: string) {
   }
 
   const imageMetadata = (await fetchedImageMetadata.json()) as ImageMetadata;
-  const extension = imageMetadata.data.type.split("/")[1];
+  let extension: string | undefined = "";
+  if (imageMetadata.data.filename_disk !== null) {
+    if (imageMetadata.data.filename_disk.split(".").length > 1) {
+      extension = imageMetadata.data.filename_disk.split(".").pop();
+    }
+  }
+  if (extension === "") {
+    if (imageMetadata.data.filename_download !== null) {
+      if (imageMetadata.data.filename_download.split(".").length > 1) {
+        extension = imageMetadata.data.filename_download.split(".").pop();
+      }
+    }
+  }
+
+  if (extension === undefined || extension === "") {
+    throw new Error("Failed to determine image extension");
+  }
 
   const stream = Readable.fromWeb(fetchedImage.body as ReadableStream);
 
   try {
     return await filesService.uploadStream(stream, extension);
   } catch (error) {
-    throw new Error(`Failed to upload image. ${error}`);
+    throw new Error(`Failed to upload image.`, { cause: error });
   }
 }
 
-export default class FaqScript {
-  static async run() {
-    await faqScript();
-  }
-}
+export default class FaqSectionScrapper extends BaseScraperModule {
+  static name = "FAQ section";
+  static description =
+    "Articles, questions, answers, and authors of the FAQ section";
+  static taskTitle = "Scrape the faq section";
 
-async function faqScript() {
-  logger.info("Fetching data...");
-  const [articlesResponse, questionsResponse, pivotTableResponse] =
-    await Promise.all([
-      fetch("https://admin.topwr.solvro.pl/items/FAQ_Types"),
-      fetch("https://admin.topwr.solvro.pl/items/FAQ"),
-      fetch("https://admin.topwr.solvro.pl/items/FAQ_Types_FAQ"),
-    ]);
+  async run() {
+    this.logger.info("Fetching data...");
+    const [articlesResponse, questionsResponse, pivotTableResponse] =
+      await Promise.all([
+        fetch("https://admin.topwr.solvro.pl/items/FAQ_Types"),
+        fetch("https://admin.topwr.solvro.pl/items/FAQ"),
+        fetch("https://admin.topwr.solvro.pl/items/FAQ_Types_FAQ"),
+      ]);
 
-  if (!articlesResponse.ok) {
-    throw new Error(
-      `Failed to fetch articles - got response status code ${articlesResponse.status}`,
-    );
-  }
-  if (!questionsResponse.ok) {
-    throw new Error(
-      `Failed to fetch questions - got response status code ${questionsResponse.status}`,
-    );
-  }
-  if (!pivotTableResponse.ok) {
-    throw new Error(
-      `Failed to fetch pivot table - got response status code ${pivotTableResponse.status}`,
-    );
-  }
-  const [articlesResult, questionsResult, pivotTableResult] = await Promise.all(
-    [
-      articlesResponse.json() as Promise<GuideArticlesOld>,
-      questionsResponse.json() as Promise<GuideQuestionsOld>,
-      pivotTableResponse.json() as Promise<PivotTable>,
-    ],
-  );
+    if (!articlesResponse.ok) {
+      throw new Error(
+        `Failed to fetch articles - got response status code ${articlesResponse.status}`,
+      );
+    }
+    if (!questionsResponse.ok) {
+      throw new Error(
+        `Failed to fetch questions - got response status code ${questionsResponse.status}`,
+      );
+    }
+    if (!pivotTableResponse.ok) {
+      throw new Error(
+        `Failed to fetch pivot table - got response status code ${pivotTableResponse.status}`,
+      );
+    }
+    const [articlesResult, questionsResult, pivotTableResult] =
+      await Promise.all([
+        articlesResponse.json() as Promise<GuideArticlesOld>,
+        questionsResponse.json() as Promise<GuideQuestionsOld>,
+        pivotTableResponse.json() as Promise<PivotTable>,
+      ]);
 
-  for (const article of articlesResult.data) {
-    let createdAt: DateTime = DateTime.now();
-    let updatedAt = DateTime.fromMillis(0);
+    for (const article of articlesResult.data) {
+      let createdAt: DateTime = DateTime.now();
+      let updatedAt = DateTime.fromMillis(0);
 
-    for (const pivot of pivotTableResult.data.filter(
-      (p) => p.FAQ_Types_id === article.id,
-    )) {
+      for (const pivot of pivotTableResult.data.filter(
+        (p) => p.FAQ_Types_id === article.id,
+      )) {
+        const question = questionsResult.data.find(
+          (q) => q.id === pivot.FAQ_id,
+        );
+
+        if (question === undefined) {
+          this.logger.warning(
+            `Pivot references missing question (ID=${pivot.FAQ_id}) for article ID=${article.id} ("${article.name}"). ` +
+              `This may be a data inconsistency in the source. Skipping this question...`,
+          );
+          continue;
+        }
+
+        const questionCreatedAt = DateTime.fromISO(question.date_created);
+        const questionUpdatedAt = DateTime.fromISO(question.date_updated);
+
+        if (questionCreatedAt < createdAt) {
+          createdAt = questionCreatedAt;
+        }
+
+        if (questionUpdatedAt > updatedAt) {
+          updatedAt = questionUpdatedAt;
+        }
+      }
+
+      const imagePath = await uploadImage(article.cover);
+
+      await GuideArticle.create({
+        id: article.id,
+        title: article.name,
+        shortDesc: article.short_description,
+        description: article.description ?? "",
+        imagePath,
+        createdAt,
+        updatedAt,
+      });
+    }
+
+    for (const pivot of pivotTableResult.data) {
+      if (pivot.FAQ_Types_id === null) {
+        continue;
+      }
       const question = questionsResult.data.find((q) => q.id === pivot.FAQ_id);
 
       if (question === undefined) {
-        logger.warn(
-          `Pivot references missing question (ID=${pivot.FAQ_id}) for article ID=${article.id} ("${article.name}"). ` +
+        this.logger.warning(
+          `Pivot references missing question (ID=${pivot.FAQ_id}) for article ID=${pivot.FAQ_Types_id}. ` +
             `This may be a data inconsistency in the source. Skipping this question...`,
         );
         continue;
       }
 
-      const questionCreatedAt = DateTime.fromISO(question.date_created);
-      const questionUpdatedAt = DateTime.fromISO(question.date_updated);
+      await GuideQuestion.create({
+        title: question.question,
+        answer: question.answer,
+        articleId: pivot.FAQ_Types_id,
+      });
+    }
 
-      if (questionCreatedAt < createdAt) {
-        createdAt = questionCreatedAt;
+    for (const question of questionsResult.data) {
+      if (!pivotTableResult.data.some((p) => p.FAQ_id === question.id)) {
+        this.logger.warning(
+          `Question (ID=${question.id}) is not referenced by any article. This may be a data inconsistency in the source. Skipping this question...`,
+        );
       }
-
-      if (questionUpdatedAt > updatedAt) {
-        updatedAt = questionUpdatedAt;
-      }
     }
 
-    const imagePath = await uploadImage(article.cover);
-
-    await GuideArticle.create({
-      id: article.id,
-      title: article.name,
-      shortDesc: article.short_description,
-      description: article.description ?? "",
-      imagePath,
-      createdAt,
-      updatedAt,
-    });
+    this.logger.info("FAQ data successfully added to the database!");
   }
-
-  for (const pivot of pivotTableResult.data.filter(
-    (p) => p.FAQ_Types_id !== null,
-  )) {
-    const question = questionsResult.data.find((q) => q.id === pivot.FAQ_id);
-
-    if (question === undefined) {
-      logger.warn(
-        `Pivot references missing question (ID=${pivot.FAQ_id}) for article ID=${pivot.FAQ_Types_id}. ` +
-          `This may be a data inconsistency in the source. Skipping this question...`,
-      );
-      continue;
-    }
-
-    await GuideQuestion.create({
-      title: question.question,
-      answer: question.answer,
-      articleId: pivot.FAQ_Types_id ?? undefined,
-    });
-  }
-
-  for (const question of questionsResult.data) {
-    if (!pivotTableResult.data.some((p) => p.FAQ_id === question.id)) {
-      logger.warn(
-        `Question (ID=${question.id}) is not referenced by any article. This may be a data inconsistency in the source. Skipping this question...`,
-      );
-    }
-  }
-
-  logger.info("FAQ data successfully added to the database!");
 }
