e9a471105e433cf25d0066ea60902e8bb12b6c35
karolwolek <karolwolek06@gmail.com>
2025-03-13 12:48:31 +0100

    refactor(auth): adjust token validation flow
    - moved ALL resest_password_token validation to the proper validator.
    - renamed the validator to avoid misconception

--- app/controllers/users_controller.ts
+++ app/controllers/users_controller.ts
@@ -4,7 +4,7 @@ import type { HttpContext } from "@adonisjs/core/http";
 import ResetPasswordService from "#services/reset_password_service";
 import { emailValidator } from "#validators/email";
 import { passwordValidator } from "#validators/password";
-import { resetPasswordTokenValidator } from "#validators/token";
+import { resetPasswordTokenValidator } from "#validators/reset_password_token";
 
 export default class UsersController {
   /**
@@ -18,7 +18,8 @@ export default class UsersController {
     const { email } = await request.validateUsing(emailValidator);
     await resetPasswordService.trySendResetUrl(email);
     return response.ok({
-      message: `If an account with that email exists, you will receive instructions to reset your password shortly.`,
+      message: `If an account with that email exists,
+      you will receive instructions to reset your password shortly.`,
     });
   }
 
@@ -30,13 +31,22 @@ export default class UsersController {
     { request, response }: HttpContext,
     resetPasswordService: ResetPasswordService,
   ) {
+    await request.validateUsing(resetPasswordTokenValidator);
+
     const {
-      params: {
-        token: { userWithToken }, // dont kill me
-      },
+      params: { token },
     } = await request.validateUsing(resetPasswordTokenValidator);
+
+    if (!token.isValid()) {
+      await resetPasswordService.destroyToken(token.user);
+      return response.unauthorized({
+        message: "Time out for request",
+      });
+    }
+
     const { password } = await request.validateUsing(passwordValidator);
-    await resetPasswordService.tryToResetForUser(userWithToken, password);
+    await resetPasswordService.tryToResetForUser(token.user, password);
+
     return response.ok({ message: "Password updated successfully" });
   }
 }
---
+++ app/validators/reset_password_token.ts
@@ -0,0 +1,83 @@
+import vine, { BaseLiteralType, VineString } from "@vinejs/vine";
+import { FieldContext } from "@vinejs/vine/types";
+import { DateTime } from "luxon";
+
+import User from "#models/user";
+
+const errorMessage = "Invalid reset token field. Request rejected";
+
+export interface ResetToken {
+  token: string;
+  user: User;
+  /**
+   * Expiration based validity. false if reset password token expiration date is less than now
+   */
+  isValid(): boolean;
+}
+
+const isValidToken = vine.createRule(
+  async (value: unknown, _, field: FieldContext) => {
+    if (!field.isValid) {
+      return;
+    }
+    const token = value as string;
+    if (!vine.helpers.isUUID(token, 4)) {
+      field.report(errorMessage, "isValidToken", field);
+      return;
+    }
+    const user = await User.query()
+      .withScopes((scopes) => {
+        scopes.compareTokens(token);
+      })
+      .first();
+    if (user === null) {
+      field.report(errorMessage, "isValidToken", field);
+      return;
+    }
+    const output: ResetToken = {
+      token,
+      user,
+      isValid: () => {
+        return true;
+      },
+    };
+    if (
+      user.resetPasswordTokenExpiration === null ||
+      user.resetPasswordTokenExpiration < DateTime.now()
+    ) {
+      output.isValid = () => {
+        return false;
+      };
+    }
+    field.mutate(output, field);
+  },
+  {
+    isAsync: true,
+  },
+);
+
+// inform ts
+declare module "@vinejs/vine" {
+  interface VineString {
+    resetPasswordToken(): BaseLiteralType<string, ResetToken, ResetToken>;
+  }
+}
+
+VineString.macro(
+  "resetPasswordToken",
+  function (this: BaseLiteralType<string, ResetToken, ResetToken>) {
+    return this.use(isValidToken());
+  },
+);
+
+/**
+ * Will fail on invalid token formats OR no user assigned to the token.
+ * Expiration date must be handled by checking `isValid()` method on output
+ */
+export const resetPasswordTokenValidator = vine.compile(
+  vine.object({
+    params: vine.object({
+      token: vine.string().resetPasswordToken(),
+    }),
+  }),
+);
