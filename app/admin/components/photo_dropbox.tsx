ae3ab1850cdb7c3d59fb2eccf99a7fa9b08faa11
Leadman5555 <149937144+Leadman5555@users.noreply.github.com>
2025-03-16 16:40:30 +0100

    feat(AdminPanel): add photo_dropbox component (#129)
    * feat: add photo_dropbox component
    
    * fix: change to preview display

---
+++ app/admin/components/photo_dropbox.tsx
@@ -0,0 +1,36 @@
+import { Box, DropZone, Label } from "@adminjs/design-system";
+import { BasePropertyProps } from "adminjs";
+import { File } from "node:buffer";
+import React, { FC } from "react";
+
+//Maximum file size allowed in dropbox in MB
+const MAX_FILE_SIZE = 1024 * 1024 * 5;
+//MIME types that the dropbox will allow to upload
+const ALLOWED_FILE_TYPES = ["image/png", "image/jpeg", "image/webp"];
+
+const PhotoDropbox: FC<BasePropertyProps> = (props) => {
+  const { property, onChange } = props;
+  if (onChange === undefined) {
+    throw new Error("Invalid dropbox configuration. onChange is not defined.");
+  }
+  const handleChange = (files: File[]): void => {
+    if (files.length > 0) {
+      onChange(property.name, files[0]);
+    } else {
+      onChange(property.name, null);
+    }
+  };
+
+  return (
+    <Box>
+      <Label>{property.label}</Label>
+      <DropZone
+        onChange={handleChange}
+        multiple={false}
+        validate={{ maxSize: MAX_FILE_SIZE, mimeTypes: ALLOWED_FILE_TYPES }}
+      />
+    </Box>
+  );
+};
+
+export default PhotoDropbox;
