161831e3aa817c16e5fbcc7b53bb13b9b52e1cef
Ignacy Smoliński <ignacy.smolinski@gmail.com>
2025-05-29 13:46:18 +0200

    refactor: changed to static version for safety
    AdonisJS creates a new controller instance with every request (why? :( )

--- app/controllers/newsfeed_controller.ts
+++ app/controllers/newsfeed_controller.ts
@@ -3,29 +3,24 @@ import { HttpContext } from "@adonisjs/core/http";
 import NewsfeedService from "#services/newsfeed_service";
 
 export default class NewsfeedController {
-  private readonly newsfeedService: NewsfeedService;
-
-  constructor() {
-    this.newsfeedService = new NewsfeedService();
-  }
-
   /**
    * Returns the latest available, first page, newsfeed articles from the PWr website
-   * as an array of {@link NewsfeedArticle}.
-   * It may return old articles if successive scraping attempts were failures.
-   * Pass 'completeOnly' query param as true in the request to only get articles that were scraped in full.
-   * @returns 200 NewsfeedArticle[] or 503 with an error message
+   * as an object with array of articles of {@link NewsfeedArticle} and update time (see {@link NewsfeedUpdate}).
+   * It may return old articles if successive scraping attempts were failures - refer to update time to judge the recency.
+   * Pass 'completeOnly=true' query param in the request to only get articles that were scraped in full (no missing fields).
+   * @returns 200 NewsfeedUpdate or 503 with an error message
    */
   async latest({ request, response }: HttpContext) {
+    await NewsfeedService.startNewsfeedUpdate();
     const completeOnly = request.qs().completeOnly === "true";
-    const articles = completeOnly
-      ? this.newsfeedService.getLatestNewsfeedArticles(true)
-      : this.newsfeedService.getLatestNewsfeedArticles();
-    if (articles === null) {
+    const update = completeOnly
+      ? NewsfeedService.getLatestNewsfeedArticles(true)
+      : NewsfeedService.getLatestNewsfeedArticles();
+    if (update === null) {
       return response.serviceUnavailable(
         "Couldn't fetch newsfeed articles. Please try again later.",
       );
     }
-    return response.ok(articles);
+    return response.ok(update);
   }
 }
