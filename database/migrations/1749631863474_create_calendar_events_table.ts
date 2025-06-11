be02a3ca1633af4b1bb59f9687b07806459427ba
Ignacy Smoliński <ignacy.smolinski@gmail.com>
2025-06-11 10:55:37 +0200

    feat: added calendar_event model, migration
    Signed-off-by: Ignacy Smoliński <ignacy.smolinski@gmail.com>

---
+++ database/migrations/1749631863474_create_calendar_events_table.ts
@@ -0,0 +1,22 @@
+import { BaseSchema } from "@adonisjs/lucid/schema";
+
+export default class extends BaseSchema {
+  protected tableName = "calendar_events";
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments("id");
+      table.text("google_cal_id").nullable();
+      table.text("name").notNullable();
+      table.timestamp("start_time").notNullable();
+      table.timestamp("end_time").notNullable();
+      table.text("location").notNullable();
+      table.timestamp("created_at");
+      table.timestamp("updated_at");
+    });
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName);
+  }
+}
