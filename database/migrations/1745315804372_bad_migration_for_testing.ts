d2658c0f91da57134e384e15ef56010149b0a2c3
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2025-06-13 18:06:31 +0200

    fix: fix the bad migration

--- database/migrations/1745315804372_bad_migration_for_testing.ts
+++ database/migrations/1745315804372_bad_migration_for_testing.ts
@@ -2,20 +2,20 @@ import { BaseSchema } from "@adonisjs/lucid/schema";
 
 export default class extends BaseSchema {
   async up() {
-    // this.defer(async (db) => {
-    await this.db.rawQuery(`
+    this.defer(async (db) => {
+      await db.rawQuery(`
         INSERT INTO student_organization_tags (tag, created_at, updated_at)
         VALUES ('test', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
       `);
-    // });
+    });
   }
 
   async down() {
-    // this.defer(async (db) => {
-    await this.db.rawQuery(`
+    this.defer(async (db) => {
+      await db.rawQuery(`
         DELETE FROM student_organization_tags
         WHERE tag = 'test';
       `);
-    // });
+    });
   }
 }
