a33d9be0e03c1ebb549e736b4d5b9e4860880ba8
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2025-06-14 13:25:32 +0200

    chore: bump @poppinss/cliui and remove @ts-expect-error

--- commands/db_scrape.ts
+++ commands/db_scrape.ts
@@ -200,7 +200,6 @@ export default class DbScrape extends BaseCommand {
     const tasks = this.ui.tasks({ verbose: true });
 
     for (const { Module, instance } of selectedModules) {
-      // @ts-expect-error https://github.com/poppinss/cliui/issues/22
       tasks.add(Module.taskTitle ?? Module.description, async (task) => {
         try {
           return ((await instance.run(task)) as string | undefined) ?? "Done";
