01a15035ca31388b34f72ec63f05c1309e8427a9
Ignacy Smoliński <ignacy.smolinski@gmail.com>
2025-06-20 11:58:15 +0200

    fix: removed unnecessary casts
    Signed-off-by: Ignacy Smoliński <ignacy.smolinski@gmail.com>

--- app/controllers/event_calendar_controller.ts
+++ app/controllers/event_calendar_controller.ts
@@ -17,7 +17,7 @@ export default class EventCalendarController extends BaseController<
   protected async storeHook(
     ctx: CreateHookContext<typeof CalendarEvent>,
   ): Promise<PartialModel<typeof CalendarEvent> | void | undefined> {
-    const event = ctx.request as unknown as CalendarEvent;
+    const event = ctx.request as CalendarEvent;
     if (event.endTime.diff(event.startTime).milliseconds <= 0) {
       throw new BadRequestException("End time must be after start time");
     }
@@ -27,8 +27,8 @@ export default class EventCalendarController extends BaseController<
   protected async updateHook(
     ctx: HookContext<typeof CalendarEvent>,
   ): Promise<PartialModel<typeof CalendarEvent> | void | undefined> {
-    const changes = ctx.request as unknown as Partial<CalendarEvent>;
-    const current = ctx.record as unknown as CalendarEvent;
+    const changes = ctx.request as Partial<CalendarEvent>;
+    const current = ctx.record;
     if (current.isGoogleEvent) {
       throw new BadRequestException("Cannot edit Google calendar events");
     }
@@ -43,84 +43,10 @@ export default class EventCalendarController extends BaseController<
   protected async destroyHook(
     ctx: DeleteHookContext<typeof CalendarEvent>,
   ): Promise<void> {
-    const event = ctx.record as unknown as CalendarEvent;
+    const event = ctx.record;
     if (event.isGoogleEvent) {
       throw new BadRequestException("Cannot delete Google calendar events");
     }
     return undefined;
   }
-  // private static validateDateTime(dateString: string) {
-  //   const dt = DateTime.fromISO(dateString);
-  //   if (!dt.isValid) {
-  //     throw new BadRequestException("Invalid date param");
-  //   }
-  //   return dt;
-  // }
-  //
-  // async index({ request }: HttpContext) {
-  //   const qs = request.qs();
-  //   const excludeGoogleEvents = qs.excludeGoogleEvents === "true";
-  //   const dateFrom: DateTime<true> | undefined =
-  //     qs.dateFrom !== undefined
-  //       ? EventCalendarController.validateDateTime(qs.dateFrom as string)
-  //       : undefined;
-  //   const dateTo: DateTime<true> | undefined =
-  //     qs.dateTo !== undefined
-  //       ? EventCalendarController.validateDateTime(qs.dateTo as string)
-  //       : undefined;
-  //   return await EventCalendarService.getEvents(
-  //     excludeGoogleEvents,
-  //     dateFrom,
-  //     dateTo,
-  //   );
-  // }
-  //
-  // async find({ params }: HttpContext): Promise<CalendarEvent> {
-  //   const eventId = params.event_id as string;
-  //   return await EventCalendarService.getEvent(eventId);
-  // }
-  //
-  // async edit({ params, request }: HttpContext): Promise<CalendarEvent> {
-  //   const eventId = params.event_id as string;
-  //   const body = request.body();
-  //   const startTime =
-  //     body.startTime !== undefined
-  //       ? EventCalendarController.validateDateTime(body.startTime as string)
-  //       : undefined;
-  //   const endTime =
-  //     body.endTime !== undefined
-  //       ? EventCalendarController.validateDateTime(body.endTime as string)
-  //       : undefined;
-  //   return await EventCalendarService.editEvent(eventId, {
-  //     ...body,
-  //     startTime,
-  //     endTime,
-  //   });
-  // }
-  //
-  // async remove({ params, response }: HttpContext): Promise<void> {
-  //   const eventId = params.event_id as string;
-  //   await EventCalendarService.removeEvent(eventId);
-  //   return response.noContent();
-  // }
-  //
-  // async create({ request, response }: HttpContext) {
-  //   const editDto = request.body();
-  //   try {
-  //     const data = await calendarEventSchemaCompiler.validate(editDto);
-  //     const result = await EventCalendarService.addEvent({
-  //       ...data,
-  //       startTime: EventCalendarController.validateDateTime(data.startTime),
-  //       endTime: EventCalendarController.validateDateTime(data.endTime),
-  //     });
-  //     return response.created(result);
-  //   } catch (error) {
-  //     if (error instanceof errors.E_VALIDATION_ERROR) {
-  //       return response.badRequest({
-  //         errors: (error as { messages: string[] }).messages,
-  //       });
-  //     }
-  //     throw error;
-  //   }
-  // }
 }
