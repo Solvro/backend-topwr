aa0b8fdd035666dbcc8c057ed05b3eb6d8d1dba8
karolwolek <karolwolek06@gmail.com>
2025-07-03 18:00:16 +0200

    feat: cache reference number (#178)
    * feat: create migration
    
    * feat: add model
    
    * feat: create scraper
    
    * feat: add controller and route
    
    * feat: integrate with admin panel
    
    * refactor: add notnullable timestamps and integer in typings
    
    * feat: add route for bumping
    
    * refactor: simplify increment
    
    * refactor: change primary type
    
    * fix: migration ordering

---
+++ app/models/cache_reference_number.ts
@@ -0,0 +1,19 @@
+import { DateTime } from "luxon";
+
+import { BaseModel } from "@adonisjs/lucid/orm";
+
+import { typedColumn } from "#decorators/typed_model";
+
+export default class CacheReferenceNumber extends BaseModel {
+  @typedColumn({ isPrimary: true, type: "integer" })
+  declare id: number;
+
+  @typedColumn({ type: "integer" })
+  declare referenceNumber: number;
+
+  @typedColumn.dateTime({ autoCreate: true })
+  declare createdAt: DateTime;
+
+  @typedColumn.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime;
+}
