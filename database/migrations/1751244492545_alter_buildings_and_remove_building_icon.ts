818195173d66926d45d060a32dcc8b9afb1876f2
Alicja Bonar <alabonar5@gmail.com>
2025-07-09 21:33:11 +0200

    fix: using defer instead of await for raw sql

--- database/migrations/1751244492545_alter_buildings_and_remove_building_icon.ts
+++ database/migrations/1751244492545_alter_buildings_and_remove_building_icon.ts
@@ -9,19 +9,23 @@ export default class extends BaseSchema {
       table.dropColumn("icon_type");
     });
 
-    await this.schema.raw("DROP TYPE IF EXISTS building_icon");
+    this.defer(() => this.db.rawQuery("DROP TYPE IF EXISTS building_icon"));
   }
 
   async down() {
-    await this.schema.raw(`
-      DO $$
-      BEGIN
-        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'building_icon') THEN
-          CREATE TYPE building_icon AS ENUM ('ICON');
-        END IF;
-      END
-      $$;
-    `);
+    this.defer(() =>
+      this.db.rawQuery(
+        `
+          DO $$
+          BEGIN
+            IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'building_icon') THEN
+              CREATE TYPE building_icon AS ENUM ('ICON');
+            END IF;
+          END
+          $$;
+        `,
+      ),
+    );
 
     this.schema.alterTable(this.tableName, (table) => {
       table
