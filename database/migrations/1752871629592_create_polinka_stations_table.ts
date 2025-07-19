045b10285a2872d6a925add5a8753e35f3e0dc8d
Alicja Bonar <alabonar5@gmail.com>
2025-07-19 18:08:20 +0200

    fix: change external digital guide to enum

--- database/migrations/1752871629592_create_polinka_stations_table.ts
+++ database/migrations/1752871629592_create_polinka_stations_table.ts
@@ -3,6 +3,12 @@ import { BaseSchema } from "@adonisjs/lucid/schema";
 export default class extends BaseSchema {
   protected tableName = "polinka_stations";
 
+  protected digitalGuideModes = [
+    "web_url",
+    "digital_guide_building",
+    "other_digital_guide_place",
+  ];
+
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments("id");
@@ -19,8 +25,16 @@ export default class extends BaseSchema {
       table.double("latitude").notNullable();
       table.double("longitude").notNullable();
       table.text("photo_key").nullable();
-      table.text("external_digital_guide_mode").nullable();
       table.text("external_digital_guide_id_or_url").nullable();
+
+      table
+        .enum("external_digital_guide_mode", this.digitalGuideModes, {
+          useNative: true,
+          enumName: "external_digital_guide_mode",
+          existingType: true,
+        })
+        .nullable();
+
       table.check(
         `
         (
