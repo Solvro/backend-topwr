a93d2d7f33a50dc207e4249deac8779ab9f885b1
Jakub Czajkowski <and.jj.an@gmail.com>
2025-08-04 20:07:06 +0200

    feat: setup adonisjs-permissions

---
+++ app/middleware/acl_middleware.ts
@@ -0,0 +1,24 @@
+import { AclManager, Scope } from "@holoyan/adonisjs-permissions";
+
+import type { HttpContext } from "@adonisjs/core/http";
+import type { NextFn } from "@adonisjs/core/types/http";
+
+declare module "@adonisjs/core/http" {
+  export interface HttpContext {
+    acl: AclManager;
+  }
+}
+
+export default class UserScopeMiddleware {
+  //@ts-ignore
+  async handle(ctx: HttpContext, next: NextFn) {
+    const scope = new Scope();
+    ctx.acl = new AclManager(true).scope(scope);
+    /**
+     * Call next method in the pipeline and return its output
+     * Call next method in the pipeline and return its output
+     */
+    const output = await next();
+    return output;
+  }
+}
