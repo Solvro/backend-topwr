2cec14e4b61f7944f60c10a79f07c3e4e27daaab
karolwolek <karolwolek06@gmail.com>
2025-08-27 12:03:10 +0200

    refactor: restore cascade where needed

--- database/migrations/1755532368818_add_restrict_rules_to_relations.ts
+++ database/migrations/1755532368818_add_restrict_rules_to_relations.ts
@@ -6,10 +6,6 @@ export default class extends BaseSchema {
     foreign: string;
     references: string;
     oldAction: string;
-    /*
-     * Adonis autogenerates this from column and table names but there is a mismatch from previous migrations
-     */
-    constraint_name?: string;
   }[] = [
     {
       table: "buildings",
@@ -36,18 +32,6 @@ export default class extends BaseSchema {
       oldAction: "SET NULL",
     },
     {
-      table: "regular_hours",
-      foreign: "library_id",
-      references: "libraries.id",
-      oldAction: "CASCADE",
-    },
-    {
-      table: "special_hours",
-      foreign: "library_id",
-      references: "libraries.id",
-      oldAction: "CASCADE",
-    },
-    {
       table: "bicycle_showers",
       foreign: "building_id",
       references: "buildings.id",
@@ -60,70 +44,9 @@ export default class extends BaseSchema {
       oldAction: "CASCADE",
     },
     {
-      table: "department_links",
-      foreign: "department_id",
-      references: "departments.id",
-      oldAction: "RESTRICT",
-      constraint_name: "departments_links_department_id_foreign", // 1744906324598 changed the name, but the constraint name stayed the same
-    },
-    {
-      table: "day_swaps",
-      foreign: "academic_calendar_id",
-      references: "academic_calendars.id",
-      oldAction: "CASCADE",
-    },
-    {
-      table: "holidays",
-      foreign: "academic_calendar_id",
-      references: "academic_calendars.id",
-      oldAction: "CASCADE",
-    },
-    {
-      table: "contributor_social_links",
-      foreign: "contributor_id",
-      references: "contributors.id",
-      oldAction: "CASCADE",
-    },
-    {
-      table: "changes",
-      foreign: "version_id",
-      references: "versions.id",
-      oldAction: "CASCADE",
-    },
-    {
-      table: "change_screenshots",
-      foreign: "change_id",
-      references: "changes.id",
-      oldAction: "CASCADE",
-    },
-    {
-      table: "version_screenshots",
-      foreign: "version_id",
-      references: "versions.id",
-      oldAction: "CASCADE",
-    },
-    {
-      table: "student_organization_links",
-      foreign: "student_organization_id",
-      references: "student_organizations.id",
-      oldAction: "CASCADE",
-    },
-    {
       table: "guide_questions",
       foreign: "article_id",
       references: "guide_articles.id",
-      oldAction: "CASCADE",
-    },
-    {
-      table: "guide_article_authors",
-      foreign: "article_id",
-      references: "guide_articles.id",
-      oldAction: "CASCADE",
-    },
-    {
-      table: "guide_article_authors",
-      foreign: "author_id",
-      references: "guide_authors.id",
       oldAction: "CASCADE",
     },
   ];
@@ -131,7 +54,7 @@ export default class extends BaseSchema {
   async up() {
     for (const constraint of this.constraints) {
       this.schema.alterTable(constraint.table, (table) => {
-        table.dropForeign(constraint.foreign, constraint.constraint_name);
+        table.dropForeign(constraint.foreign);
         table
           .foreign(constraint.foreign)
           .references(constraint.references)
@@ -145,7 +68,7 @@ export default class extends BaseSchema {
       this.schema.alterTable(constraint.table, (table) => {
         table.dropForeign(constraint.foreign);
         table
-          .foreign(constraint.foreign, constraint.constraint_name)
+          .foreign(constraint.foreign)
           .references(constraint.references)
           .onDelete(constraint.oldAction);
       });
