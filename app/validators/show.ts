e5a9fe1d4100a27334c2dcbc115e34f4c53f1d86
karolwolek <karolwolek06@gmail.com>
2025-09-16 16:27:45 +0200

    refactor: remove unused show validator

--- app/validators/show.ts
+++
@@ -1,9 +0,0 @@
-import vine from "@vinejs/vine";
-
-export const showValidator = vine.compile(
-  vine.object({
-    params: vine.object({
-      id: vine.number().min(1).withoutDecimals(),
-    }),
-  }),
-);
