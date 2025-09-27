72a2a3c4af34632c4dbba84458c2974dfe26252b
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2025-09-27 18:21:25 +0200

    feat(db): autocompute order fields with SQL triggers

--- database/migrations/1758982174631_add_order_fields.ts
+++ database/migrations/1758982174631_add_order_fields.ts
@@ -4,6 +4,22 @@ export default class extends BaseSchema {
   protected tables = ["contributors", "guide_articles", "guide_questions"];
 
   async up() {
+    this.schema.raw(`
+      CREATE FUNCTION order_autocompute() RETURNS trigger AS $$
+        BEGIN
+          IF NEW.order IS NULL THEN
+            EXECUTE format('SELECT MAX("order") + 1 FROM %I', TG_TABLE_NAME)
+            INTO STRICT NEW.order;
+
+            IF NEW.order IS NULL THEN
+              NEW.order := 1;
+            END IF;
+          END IF;
+
+          RETURN NEW;
+        END;
+      $$ LANGUAGE plpgsql
+    `);
     for (const tableName of this.tables) {
       this.schema.raw(
         `
@@ -15,6 +31,11 @@ export default class extends BaseSchema {
 
         ALTER TABLE "${tableName}"
           ALTER COLUMN "order" SET NOT NULL;
+
+        CREATE TRIGGER order_autocompute
+          BEFORE INSERT ON "${tableName}"
+          FOR EACH ROW
+          EXECUTE FUNCTION order_autocompute();
       `,
       );
     }
@@ -26,5 +47,6 @@ export default class extends BaseSchema {
         table.dropColumn("order");
       });
     }
+    this.schema.raw("DROP FUNCTION order_autocompute() CASCADE");
   }
 }
