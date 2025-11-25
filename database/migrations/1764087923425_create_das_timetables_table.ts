f4e35efcddb11d73d53d274f93fb38cc33d3af5e
Leadman5555 <ignacy.smolinski@gmail.com>
2025-11-25 18:27:15 +0100

    feat: das schema

---
+++ database/migrations/1764087923425_create_das_timetables_table.ts
@@ -0,0 +1,23 @@
+import { BaseSchema } from "@adonisjs/lucid/schema";
+
+export default class extends BaseSchema {
+  protected tableName = "das_timetables";
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table
+        .integer("id")
+        .references("id")
+        .inTable("das")
+        .primary()
+        .onDelete("CASCADE");
+      table.text("name").notNullable();
+      table.timestamp("created_at").notNullable();
+      table.timestamp("updated_at").notNullable();
+    });
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName);
+  }
+}
---
+++ database/migrations/1764089017312_create_das_maps_table.ts
@@ -0,0 +1,31 @@
+import { BaseSchema } from "@adonisjs/lucid/schema";
+
+export default class extends BaseSchema {
+  protected tableName = "das_maps";
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments("id").notNullable();
+      table
+        .integer("das_id")
+        .references("id")
+        .inTable("das")
+        .notNullable()
+        .onDelete("CASCADE");
+      table.primary(["das_id", "id"]);
+      table.string("name", 127).notNullable();
+      table
+        .uuid("content_key")
+        .references("id")
+        .inTable("file_entries")
+        .notNullable()
+        .onDelete("RESTRICT");
+      table.timestamp("created_at").notNullable();
+      table.timestamp("updated_at").notNullable();
+    });
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName);
+  }
+}
