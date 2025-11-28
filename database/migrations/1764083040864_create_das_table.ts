9d8c46f0768f65399e4ad2444371bf1dd56877ec
Leadman5555 <ignacy.smolinski@gmail.com>
2025-11-28 11:58:54 +0100

    feat: added DAS start/end dates

--- database/migrations/1764083040864_create_das_table.ts
+++ database/migrations/1764083040864_create_das_table.ts
@@ -6,6 +6,8 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments("id").primary();
+      table.timestamp("starts_at").notNullable();
+      table.timestamp("ends_at").notNullable();
       table.timestamp("created_at").notNullable();
     });
   }
