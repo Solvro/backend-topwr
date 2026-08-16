bc95c8096abfcccb2864580ef5b6c60daf6bead6
krzys13 <gumek09@gmail.com>
2026-08-16 21:01:03 +0200

    feat: implement Fields of Study scraper with Cheerio integration
    Remove field of study scraping part from departments  scraper
    Create separate field of study scraper
    Samll changes in db_scraper scraper framework

--- database/scrapers/departments.ts
+++ database/scrapers/departments.ts
@@ -3,10 +3,10 @@ import { DateTime } from "luxon";
 import { BaseScraperModule } from "#commands/db_scrape";
 import type { SourceResponse, TaskHandle } from "#commands/db_scrape";
 import { Branch } from "#enums/branch";
-import { mapToStudiesType } from "#enums/studies_type";
+
 import DepartmentModel from "#models/department";
 import DepartmentLinkModel from "#models/department_link";
-import FieldOfStudyModel from "#models/field_of_study";
+
 import { fixSequence } from "#utils/db";
 
 interface DepartmentsDraft {
@@ -21,17 +21,6 @@ interface DepartmentsDraft {
   betterCode: string;
 }
 
-interface FieldOfStudyDraft {
-  id: number;
-  department_id: number | null;
-  name: string;
-  url: string | null;
-  isEnglish: boolean;
-  is2ndDegree: boolean;
-  isLongCycleStudies: boolean;
-  hasWeekendModeOption: boolean;
-}
-
 interface DepartmentLinkDraft {
   id: number;
   department_id: number | null;
@@ -41,42 +30,43 @@ interface DepartmentLinkDraft {
 }
 
 export default class DepartmentsScraper extends BaseScraperModule {
+<<<<<<< HEAD
   static readonly name = "Departments";
   static readonly description =
     "Scrapes pwr departments, field of study and departments links data from directus";
   static readonly taskTitle =
     "Scrape departments, field of study and departments links";
+=======
+  static name = "Departments";
+  static description =
+    "Scrapes pwr departments and departments links data from directus";
+  static taskTitle = "Scrape departmentsand departments links";
+>>>>>>> 491b612 (feat: implement Fields of Study scraper with Cheerio integration)
 
-  private readonly directusSchemas = [
-    "Departments",
-    "FieldOfStudy",
-    "Departments_Links",
-  ];
+  private readonly directusSchemas = ["Departments", "Departments_Links"];
   private readonly addressRegex =
     /\b\d{2}-\d{3}\s+[A-ZĄĆĘŁŃÓŚŹŻa-ząćęłńóśźż-]+$/;
 
   async shouldRun(): Promise<boolean> {
-    return await this.modelHasNoRows(DepartmentModel, FieldOfStudyModel);
+    return await this.modelHasNoRows(DepartmentModel);
   }
 
   async run(task: TaskHandle) {
     task.update("Starting fetching all schema objects");
 
-    const [departmentsData, fieldOfStudyData, departmentLinkData] =
-      (await Promise.all(
-        this.directusSchemas.map((schema) =>
-          this.semaphore.runTask(() =>
-            this.fetchDirectusJSON(
-              `https://admin.topwr.solvro.pl/items/${schema}?limit=-1`,
-              schema,
-            ),
+    const [departmentsData, departmentLinkData] = (await Promise.all(
+      this.directusSchemas.map((schema) =>
+        this.semaphore.runTask(() =>
+          this.fetchDirectusJSON(
+            `https://admin.topwr.solvro.pl/items/${schema}?limit=-1`,
+            schema,
           ),
         ),
-      )) as [
-        SourceResponse<DepartmentsDraft>,
-        SourceResponse<FieldOfStudyDraft>,
-        SourceResponse<DepartmentLinkDraft>,
-      ];
+      ),
+    )) as [
+      SourceResponse<DepartmentsDraft>,
+      SourceResponse<DepartmentLinkDraft>,
+    ];
     const departmentEntries = await Promise.all(
       departmentsData.data.map(async (departmentEntry) => {
         // Logo
@@ -127,35 +117,6 @@ export default class DepartmentsScraper extends BaseScraperModule {
     task.update(
       `Departments created, ID sequence updated to ${nextDepartmentId}`,
     );
-
-    await FieldOfStudyModel.createMany(
-      fieldOfStudyData.data.flatMap((data) => {
-        if (data.department_id === null) {
-          this.logger.warning(
-            `Skipped field of study entry ${data.id} due to missing department_id.`,
-          );
-          return [];
-        }
-        return [
-          {
-            id: data.id,
-            departmentId: data.department_id,
-            name: data.name,
-            url: data.url,
-            isEnglish: data.isEnglish,
-            studiesType: mapToStudiesType(
-              data.isLongCycleStudies,
-              data.is2ndDegree,
-            ),
-            hasWeekendOption: data.hasWeekendModeOption,
-            createdAt: DateTime.now(),
-            updatedAt: DateTime.now(),
-          },
-        ];
-      }),
-    );
-    const nextFosId = await fixSequence("fields_of_studies");
-    task.update(`Fields of Study created, ID sequence updated to ${nextFosId}`);
 
     await DepartmentLinkModel.createMany(
       departmentLinkData.data.flatMap((linkEntry) => {
---
+++ database/scrapers/fields_of_study.ts
@@ -0,0 +1,225 @@
+import { BaseScraperModule } from "#commands/db_scrape";
+import type { TaskHandle } from "#commands/db_scrape";
+import Department from "#models/department";
+import FieldOfStudyModel from "#models/field_of_study";
+
+import * as cheerio from "cheerio";
+import { mapToStudiesType } from "#app/enums/studies_type";
+
+type FieldOfStudyDetailKey = Exclude<keyof FieldOfStudyDetails, "url" | "name">;
+
+const LABELS: Partial<Record<string, FieldOfStudyDetailKey>> = {
+  "forma studiów": "studyForm",
+  "czas trwania": "timeSpan",
+  czesne: "priceForPolishCitizens",
+  "język wykładowy": "language",
+  miasto: "city",
+  "profil kierunku": "fieldProfile",
+  dyscyplina: "discipline",
+};
+
+interface FieldOfStudyDetails {
+  url: string;
+  name: string;
+  department: string;
+  studyForm: string;
+  timeSpan: string;
+  priceForPolishCitizens: string;
+  priceForForeignCitizens: string;
+  language: string;
+  city: string;
+  fieldProfile: string;
+  discipline: string;
+}
+
+interface FieldOfStudyResponse {
+  html: string;
+}
+
+export default class FieldsOfStudyScraper extends BaseScraperModule {
+  static name = "Fields of study";
+  static description = "Scrape fields of study";
+  static taskTitle? = "Scrape fields of study";
+
+  private readonly url: string =
+    "https://rekrutacja.pwr.edu.pl/wp-admin/admin-ajax.php";
+  private readonly basicInit: RequestInit = {
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      action: "getFilteredStudies",
+      s_level: "s2",
+    }),
+    method: "POST",
+  };
+  private readonly firstDegreeStudyLevelParam = "s1";
+  private readonly secondDegreeStudyLevelParam = "s2";
+  private readonly shortendFirstDegreeTermSpan = 6;
+  private readonly basicFirstDegreeTermSpan = 7;
+  private readonly extendedFirstDegreeTermSpan = 8;
+
+  private readonly firstDegreeTermSpans = [
+    this.shortendFirstDegreeTermSpan,
+    this.basicFirstDegreeTermSpan,
+    this.extendedFirstDegreeTermSpan,
+  ];
+  private readonly basicLongCycleTermSpan = 10;
+  private readonly extendedLongCycleTermSpan = 12;
+
+  private readonly longCycleTermSpans = [
+    this.basicLongCycleTermSpan,
+    this.extendedLongCycleTermSpan,
+  ];
+  private readonly basicSecondDegreeTermSpan = 3;
+  private readonly extendedSecondDegreeTermSpan = 4;
+  private readonly secondDegreeTermSpans = [
+    this.basicSecondDegreeTermSpan,
+    this.extendedSecondDegreeTermSpan,
+  ];
+  private extendInit(studyLevel: string) {
+    const body = this.basicInit.body as URLSearchParams;
+    body.set("s_level", studyLevel);
+    return Object.assign(this.basicInit, body);
+  }
+
+  private async mapDetailsToModel(
+    details: FieldOfStudyDetails,
+  ): Promise<FieldOfStudyModel> {
+    const fieldOfStudy = new FieldOfStudyModel();
+    let department;
+    if (details.name === "Lekarski") {
+      department = await Department.findBy("name", "Wydział Medyczny"); // this is temporary fix for the fact that the department name is not present in the details for the field of study "Lekarski"
+    } else {
+      department = await Department.findBy("name", details.department);
+    }
+    if (department === null) {
+      throw new Error(
+        `Department "${details.department}" not found for field of study "${details.name}."`,
+      );
+    }
+
+    fieldOfStudy.departmentId = department.id;
+    fieldOfStudy.name = details.name;
+    fieldOfStudy.url = details.url;
+    fieldOfStudy.isEnglish = details.language === "angielski";
+    if (
+      this.firstDegreeTermSpans.some((value) =>
+        details.timeSpan.includes(value.toString()),
+      )
+    ) {
+      fieldOfStudy.studiesType = mapToStudiesType(false, false);
+    } else if (
+      this.longCycleTermSpans.some((value) =>
+        details.timeSpan.includes(value.toString()),
+      )
+    ) {
+      fieldOfStudy.studiesType = mapToStudiesType(true, false);
+    } else if (
+      this.secondDegreeTermSpans.some((value) =>
+        details.timeSpan.includes(value.toString()),
+      )
+    ) {
+      fieldOfStudy.studiesType = mapToStudiesType(false, true);
+    } else {
+      throw new Error(
+        `Field of study has unknown studies type. name=${details.name}, department=${details.department}, timeSpan=${details.timeSpan}, language=${details.language}.`,
+      );
+    }
+    fieldOfStudy.hasWeekendOption =
+      details.priceForPolishCitizens.includes("niestacjonarne");
+
+    return fieldOfStudy;
+  }
+
+  private async scrapeFieldOfStudy(url: string, init: RequestInit) {
+    const res: FieldOfStudyResponse = (await this.fetchJSON(
+      url,
+      "html with field of study data",
+      init,
+    )) as FieldOfStudyResponse;
+
+    const $ = cheerio.load(res.html);
+
+    const result = $.extract({
+      fieldsOfStudyDetails: [
+        {
+          selector: "div.col-12",
+          value: {
+            name: "h2",
+            url: {
+              selector: "a.study-card",
+              value: "href",
+            },
+          },
+        },
+      ],
+    });
+    const fieldsOfStudyDetails =
+      result.fieldsOfStudyDetails as unknown as FieldOfStudyDetails[];
+
+    for (const details of fieldsOfStudyDetails) {
+      const responseDetails = await fetch(details.url);
+      const htmlDetails = await responseDetails.text();
+      const detailPage = cheerio.load(htmlDetails);
+
+      details.department = detailPage("div.course-header__top a > span")
+        .text()
+        .trim();
+
+      for (const wrapper of detailPage(
+        ".course-data-bar__data-wrapper",
+      ).toArray()) {
+        const label = detailPage(wrapper)
+          .find("h3")
+          .text()
+          .trim()
+          .toLocaleLowerCase("pl");
+        const value = detailPage(wrapper).find("li").text().trim();
+
+        const key = LABELS[label];
+        if (key) {
+          details[key] = value;
+        }
+      }
+      let fieldOfStudy;
+      try {
+        fieldOfStudy = await this.mapDetailsToModel(details);
+      } catch (error) {
+        this.logger.warning(
+          `Mapping to model error. ${error} Insertion of this field of study skipped`,
+        );
+        continue;
+      }
+      const existingFieldOfStudy = await FieldOfStudyModel.findBy(
+        "name",
+        fieldOfStudy.name,
+      );
+
+      try {
+        if (existingFieldOfStudy !== null) {
+          existingFieldOfStudy.merge(fieldOfStudy.$attributes);
+          await existingFieldOfStudy.save();
+        } else {
+          await fieldOfStudy.save();
+        }
+      } catch (error) {
+        this.logger.warning(
+          `${error} Insertion of this field of study skipped`,
+        );
+      }
+    }
+  }
+  async run(task: TaskHandle): Promise<string> {
+    task.update("Starting fetching all fields of study");
+    await this.scrapeFieldOfStudy(
+      this.url,
+      this.extendInit(this.firstDegreeStudyLevelParam),
+    );
+    await this.scrapeFieldOfStudy(
+      this.url,
+      this.extendInit(this.secondDegreeStudyLevelParam),
+    );
+    return "Done";
+  }
+}
